fb193060228494f5bf507c0f3a8d1defc4ffefe8
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 00:47:13 +0100

    Add project files.

---
+++ LiveForexRatesAlertWebScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiveForexRatesAlertWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
