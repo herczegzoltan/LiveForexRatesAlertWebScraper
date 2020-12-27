d6c9d2a97d8a5246e81711eb9871b1408de0e43a
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 19:34:53 +0100

    Add extension for checking is IEnumerable NullOrEmpty

---
+++ LiveForexRatesAlertWebScraper/Infrastructure/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiveForexRatesAlertWebScraper.Infrastructure
+{
+    public static class HasAnyElement
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+    }
+}
