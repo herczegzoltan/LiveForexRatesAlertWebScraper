fe5efaaed6da455553fd43d84248ecf1b16c0cf2
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 20:33:16 +0100

    Refactroring and Update way to send messages via email

--- LiveForexRatesAlertWebScraper/Program.cs
+++ LiveForexRatesAlertWebScraper/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
-using System.Threading.Channels;
 using System.Timers;
-using AngleSharp;
-using AngleSharp.Css;
 using LiveForexRatesAlertWebScraper.Infrastructure;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace LiveForexRatesAlertWebScraper
 {
@@ -19,11 +12,12 @@ namespace LiveForexRatesAlertWebScraper
 
         static void Main(string[] args)
         {
-            SetTimer();
-            ValidationForCheckCurrencyAlerts();
+            bool isValid = ValidationForCheckCurrencyAlerts();
 
-            ////EmailSender emailSender = new EmailSender();
-            ////emailSender.test();
+            if (isValid)
+            {
+                SetTimer();
+            }
 
             Console.WriteLine("\nPress the Enter key to exit the application...\n");
             Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
@@ -32,49 +26,71 @@ namespace LiveForexRatesAlertWebScraper
             _timer.Dispose();
         }
 
-        static void CheckCurrencyValuesAndSendAlert(object sender, ElapsedEventArgs e)
+        static void CheckAlertCriterion(object sender, ElapsedEventArgs e)
         {
-            Console.Clear();
-            var results = WebScraper.GetCurrencyPairs(webSite);
+            var alerts = Config.Get().alerts;
+            
+            var webScrapeCurrencyPairs = WebScraper.GetCurrencyPairs(webSite).Result.AsEnumerable();
 
-            foreach (var item in results.Result)
+            var alertCurrencyPairs  = 
+                from currency in webScrapeCurrencyPairs
+                from alert in alerts
+                where (currency.Name == alert.name && 
+                       ((currency.BidPrice < alert.price && alert.direction == "-") ||
+                      (currency.BidPrice > alert.price && alert.direction == "+")))
+                select currency;
+
+            if (!alertCurrencyPairs.IsNullOrEmpty())
             {
-                Console.WriteLine(item.Name + " " + item.BidPrice + " " + item.AskPrice);
+                string message = "";
+
+                foreach (var item in alertCurrencyPairs)
+                {
+                    message = item.Name + " hit alert value at " + item.BidPrice  + "\n" + message;
+                }
+                EmailSender emailSender = new EmailSender(message);
+                emailSender.Send();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("Email was sent at {0:HH: mm: ss.fff}", DateTime.Now + "\n" + message + "\n");
+
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
         
-        static void ValidationForCheckCurrencyAlerts()
+        static bool ValidationForCheckCurrencyAlerts()
         {
             var results = WebScraper.GetCurrencyPairs(webSite);
 
-            var alerts = Config.Get().GetSection("alerts").GetChildren().ToArray();
+            var alerts = Config.Get().alerts;
 
+            bool isValid = false;
+            
             foreach (var item in alerts)
             {
-                var result = results.Result.Exists(m => m.Name == item.Key);
+                isValid = results.Result.Exists(m => m.Name == item.name);
 
-                if (!result)
+                if (!isValid)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
                     Console.WriteLine("Invalid price alert in appsettings.json!");
-                    _timer.Stop();
-                }
-                else
-                {
-                    Console.WriteLine("Valid price alert in appsettings.json!");
                 }
             }
-            
-            foreach (var item in results.Result)
-            {
-                Console.WriteLine(item.Name + " " + item.BidPrice + " " + item.AskPrice);
-            }
-         
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Valid price alerts were found in appsettings.json!");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return isValid;
         }
-        
+
         static void SetTimer()
         {
-            _timer = new System.Timers.Timer(5000);
-            _timer.Elapsed += CheckCurrencyValuesAndSendAlert;
+            _timer = new System.Timers.Timer(10000);
+            _timer.Elapsed += CheckAlertCriterion;
             _timer.AutoReset = true;
             _timer.Enabled = true;
         }
