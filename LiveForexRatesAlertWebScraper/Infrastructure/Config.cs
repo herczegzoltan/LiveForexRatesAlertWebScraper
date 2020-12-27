5155ff42ff778a7d91d68da1317fe1a8f5669d58
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 15:55:10 +0100

    Fix Configuration file reading issue

---
+++ LiveForexRatesAlertWebScraper/Infrastructure/Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Org.BouncyCastle.Asn1.X509;
+
+namespace LiveForexRatesAlertWebScraper.Infrastructure
+{
+    public static class Config
+    {
+        private static IConfigurationRoot configuration;
+        static Config()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false);
+            configuration = builder.Build();
+        }
+
+        public static IConfigurationRoot Get()
+        {
+            var appSettings = configuration;
+            
+            return appSettings;
+        }
+    }
+}
