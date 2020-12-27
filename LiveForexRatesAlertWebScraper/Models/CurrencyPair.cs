e612b38043902f89463986239b07b372ce327b4e
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 13:05:48 +0100

    Add currency model

---
+++ LiveForexRatesAlertWebScraper/Models/CurrencyPair.cs
@@ -0,0 +1,11 @@
+﻿namespace LiveForexRatesAlertWebScraper.Models
+{
+    public class CurrencyPair
+    {
+        public string Name { get; set; }
+
+        public float BidPrice { get; set; }
+     
+        public float AskPrice { get; set; }
+    }
+}
