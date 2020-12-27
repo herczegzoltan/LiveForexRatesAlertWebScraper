6c3bb303af9ddcb9d7eefe22b46a695891242597
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 18:19:19 +0100

    Add Settings class and load json into, update Config class

--- LiveForexRatesAlertWebScraper/Infrastructure/Config.cs
+++ LiveForexRatesAlertWebScraper/Infrastructure/Config.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using LiveForexRatesAlertWebScraper.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Org.BouncyCastle.Asn1.X509;
@@ -10,20 +11,20 @@ namespace LiveForexRatesAlertWebScraper.Infrastructure
 {
     public static class Config
     {
-        private static IConfigurationRoot configuration;
-        static Config()
+        private static IConfigurationRoot _configuration;
+        
+        public static Settings Get()
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
                 .AddJsonFile("appsettings.json", false);
-            configuration = builder.Build();
-        }
+            _configuration = builder.Build();
 
-        public static IConfigurationRoot Get()
-        {
-            var appSettings = configuration;
-            
-            return appSettings;
+
+            var section = _configuration.GetSection(nameof(Settings));
+            var settingsConfig = section.Get<Settings>();
+
+            return settingsConfig;
         }
     }
 }
---
+++ LiveForexRatesAlertWebScraper/Models/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveForexRatesAlertWebScraper.Models
+{
+    public class Alert
+    {
+        public string name { get; set; }
+        public string price { get; set; }
+        public string direction { get; set; }
+    }
+
+    public class Sender
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Email
+    {
+        public string HostAddress { get; set; }
+        public int HostPort { get; set; }
+        public Sender Sender { get; set; }
+        public string RecipientEmail { get; set; }
+    }
+
+    public class Settings
+    {
+        public List<Alert> alerts { get; set; }
+        public Email Email { get; set; }
+    }
+}
