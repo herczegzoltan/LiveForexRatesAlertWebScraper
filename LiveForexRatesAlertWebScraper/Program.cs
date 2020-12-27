7577520dd5de417744a49daadbe0fb948fb0951f
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 13:19:52 +0100

    Add timer to call GetCurrencyPairs

--- LiveForexRatesAlertWebScraper/Program.cs
+++ LiveForexRatesAlertWebScraper/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Timers;
+using AngleSharp.Css;
 using LiveForexRatesAlertWebScraper.Infrastructure;
 
 namespace LiveForexRatesAlertWebScraper
@@ -6,8 +8,20 @@ namespace LiveForexRatesAlertWebScraper
     class Program
     {
         static string webSite = "https://www.investing.com/currencies/streaming-forex-rates-majors";
+        private static Timer aTimer;
 
         static void Main(string[] args)
+        {
+            SetTimer();
+
+            Console.WriteLine("\nPress the Enter key to exit the application...\n");
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            Console.ReadLine();
+            aTimer.Stop();
+            aTimer.Dispose();
+        }
+
+        static void PrintCurrenciesToConsole(object sender, ElapsedEventArgs e)
         {
             var results = WebScraper.GetCurrencyPairs(webSite);
 
@@ -15,6 +29,14 @@ namespace LiveForexRatesAlertWebScraper
             {
                 Console.WriteLine(item.Name);
             }
+        }
+        
+        private static void SetTimer()
+        {
+            aTimer = new System.Timers.Timer(5000);
+            aTimer.Elapsed += PrintCurrenciesToConsole;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
         }
     }
 }
