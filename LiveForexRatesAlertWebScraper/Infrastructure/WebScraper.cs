151f195cfa3d1fa415f9fa5169b2718040b1d895
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 13:05:36 +0100

    Add webscraper

---
+++ LiveForexRatesAlertWebScraper/Infrastructure/WebScraper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using LiveForexRatesAlertWebScraper.Models;
+
+namespace LiveForexRatesAlertWebScraper.Infrastructure
+{
+    public static class WebScraper
+    {
+        public static async Task<List<CurrencyPair>> GetCurrencyPairs(string webSite)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(webSite);
+
+            // Only for debuggging
+            // _logger.LogInformation(document.DocumentElement.OuterHtml);
+
+            var instrumentTilesRows = document.GetElementById("cr1");
+
+            var tbody = instrumentTilesRows.QuerySelectorAll("tr");
+
+            var currencyPairsList = new List<CurrencyPair>();
+
+            foreach (var row in tbody)
+            {
+                var currencyPair = new CurrencyPair();
+
+                var temp = row.QuerySelectorAll("td");
+
+                if (temp.Length != 0)
+                {
+                    currencyPair.Name = temp[1].TextContent;
+                    currencyPair.BidPrice = float.Parse(temp[2].TextContent);
+                    currencyPair.AskPrice = float.Parse(temp[3].TextContent);
+
+                    currencyPairsList.Add(currencyPair);
+                }
+            }
+
+            return currencyPairsList;
+        }
+        
+    }
+}
