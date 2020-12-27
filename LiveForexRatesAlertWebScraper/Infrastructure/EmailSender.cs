416872bd361a59eb49b89a0e96efe0e6c16bfd5e
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 16:43:31 +0100

    Add email sending method and call

---
+++ LiveForexRatesAlertWebScraper/Infrastructure/EmailSender.cs
@@ -0,0 +1,54 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using MailKit.Net.Smtp;
+using MailKit;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace LiveForexRatesAlertWebScraper.Infrastructure
+{
+    public class EmailSender
+    {
+        private const string V = "Title";
+        
+        public async void test()
+        {
+            var sett = Config.Get();
+
+            string mailserviceaddress = Config.Get().GetSection("Email:HostAddress").Value;
+            int mailserviceport = 587;
+            string senderemail = Config.Get().GetSection("Email:Sender:Email").Value;
+            string senderpassword = Config.Get().GetSection("Email:Sender:Password").Value;
+            string recipientemail = Config.Get().GetSection("Email:RecipientEmail").Value;
+
+            var msg = new MimeMessage();
+            msg.From.Add(new MailboxAddress("Web Scraper", "price-alert-noreply@localhost.com"));
+            msg.To.Add(new MailboxAddress("Me", recipientemail));
+
+            msg.Subject = "this is subject";
+            msg.Body = new TextPart("plain") { Text = "msgBody" };
+
+            try
+            {
+                using (var client = new SmtpClient())
+                {
+                    client.Connect(mailserviceaddress, mailserviceport, MailKit.Security.SecureSocketOptions.StartTls);
+                    client.Authenticate(senderemail, senderpassword);
+
+                    await client.SendAsync(msg);
+                    client.Disconnect(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+        }
+    }
+}
--- LiveForexRatesAlertWebScraper/Program.cs
+++ LiveForexRatesAlertWebScraper/Program.cs
@@ -1,42 +1,51 @@
 ﻿using System;
+using System.IO;
 using System.Timers;
+using AngleSharp;
 using AngleSharp.Css;
 using LiveForexRatesAlertWebScraper.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace LiveForexRatesAlertWebScraper
 {
     class Program
     {
-        static string webSite = "https://www.investing.com/currencies/streaming-forex-rates-majors";
-        private static Timer aTimer;
+        static readonly string webSite = "https://www.investing.com/currencies/streaming-forex-rates-majors";
+        private static Timer _timer;
 
         static void Main(string[] args)
         {
             SetTimer();
 
+            EmailSender emailSender = new EmailSender();
+            emailSender.test();
+            
             Console.WriteLine("\nPress the Enter key to exit the application...\n");
             Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
             Console.ReadLine();
-            aTimer.Stop();
-            aTimer.Dispose();
+            _timer.Stop();
+            _timer.Dispose();
         }
 
         static void PrintCurrenciesToConsole(object sender, ElapsedEventArgs e)
         {
+            Console.Clear();
             var results = WebScraper.GetCurrencyPairs(webSite);
 
             foreach (var item in results.Result)
             {
-                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Name + " " + item.BidPrice + " " + item.AskPrice);
             }
         }
         
         private static void SetTimer()
         {
-            aTimer = new System.Timers.Timer(5000);
-            aTimer.Elapsed += PrintCurrenciesToConsole;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
+            _timer = new System.Timers.Timer(5000);
+            _timer.Elapsed += PrintCurrenciesToConsole;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
         }
     }
 }
