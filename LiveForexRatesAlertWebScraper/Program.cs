f58a695aee76bda700fd0c1657e4bf42abb94ccc
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-27 13:06:07 +0100

    Call webscraper in the Main

--- LiveForexRatesAlertWebScraper/Program.cs
+++ LiveForexRatesAlertWebScraper/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using LiveForexRatesAlertWebScraper.Infrastructure;
 
 namespace LiveForexRatesAlertWebScraper
 {
     class Program
     {
+        static string webSite = "https://www.investing.com/currencies/streaming-forex-rates-majors";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var results = WebScraper.GetCurrencyPairs(webSite);
+
+            foreach (var item in results.Result)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
     }
 }
